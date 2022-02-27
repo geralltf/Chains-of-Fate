2378f9a16f86834ab1576d53328840b2a09cb478
tim pearson <1018360@student.sae.edu.au>
2022-02-28 00:45:17 +1100

    Added Movement

--- Assets/Team Members/TimP/PlayerController.cs
+++ Assets/Team Members/TimP/PlayerController.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //public PlayerInput playerInput;
+    private CoFPlayerControls controls;
+    private Vector2 move;
+    public float speed = 10f;
+    private void Awake()
     {
-        
+        controls = new CoFPlayerControls();
+        controls.Player.Movement.performed += ctx => move = ctx.ReadValue<Vector2>(); //on button press gets the movement value and starts the movement
+        controls.Player.Movement.canceled += ctx => move = Vector2.zero; //on button release stops movement
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Player.Disable();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(move.x, 0.0f, move.y)*speed*Time.deltaTime; //gets a value based on actual seconds not pc specs that is used to calculate movement
+        transform.Translate(movement,Space.World);
     }
 }
