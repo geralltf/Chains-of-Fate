3561d6218e04a15dab114f0875531737b72d7ea4
Gerallt Franke <geralltf@gmail.com>
2022-03-23 15:54:40 +1100

    [Gerallt] - Provide a means of bringing in the current enemies to the combat scene.

--- Assets/Team Members/Gerallt/Scripts/ViewModel/CombatUI.cs
+++ Assets/Team Members/Gerallt/Scripts/ViewModel/CombatUI.cs
@@ -6,6 +6,15 @@ namespace ChainsOfFate.Gerallt
 {
     public class CombatUI : MonoBehaviour
     {
+        public List<GameObject> CurrentEnemies;
+
+        public void SetCurrentEnemies(List<GameObject> enemies)
+        {
+            CurrentEnemies = enemies;
+            
+            Debug.Log("Got list of current enemies");
+        }
+        
         // Start is called before the first frame update
         void Start()
         {
