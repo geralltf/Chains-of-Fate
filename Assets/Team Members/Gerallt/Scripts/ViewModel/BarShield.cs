6252da95c419246a76315387bdab5b8551b5a9ab
Gerallt Franke <geralltf@gmail.com>
2022-04-04 12:03:11 +1000

    Cleaned up code and removed hack that fixed rotation.

--- Assets/Team Members/Gerallt/Scripts/ViewModel/BarShield.cs
+++ Assets/Team Members/Gerallt/Scripts/ViewModel/BarShield.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Mathematics;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace ChainsOfFate.Gerallt
@@ -10,10 +5,9 @@ namespace ChainsOfFate.Gerallt
     public class BarShield : MonoBehaviour
     {
         public BlockBarUI ParentView;
-
-        //public event Action onHitSquare;
-
         public BarHitSquare currentTarget = null;
+        
+        //public event Action onHitSquare;
         
         private void OnCollisionEnter(Collision collision)
         {
@@ -35,18 +29,6 @@ namespace ChainsOfFate.Gerallt
             {
                 currentTarget = null;
             }
-        }
-
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            transform.rotation = quaternion.identity; //HACK
         }
     }
 
