9c4970ea199e4d3a5e53875bce02d372a25b37c8
Gerallt Franke <geralltf@gmail.com>
2022-04-16 23:40:07 +1000

    All around better loader script with support to additively load the 'Managers Scene'

--- Assets/Team Members/TimP/Loader.cs
+++ Assets/Team Members/TimP/Loader.cs
@@ -6,12 +6,41 @@ using UnityEngine.SceneManagement;
 
 public class Loader : MonoBehaviour
 {
+    public string mainScene = "Main";
+    public string managersScene = "Managers Scene";
+    public bool loadMainScene = false;
+
+    private Scene thisScene;
+    
     private void Awake()
     {
-        DontDestroyOnLoad(FindObjectOfType<ChainsOfFate.Gerallt.GameManager>().gameObject);
+        thisScene = SceneManager.GetActiveScene();
+        
         DontDestroyOnLoad(FindObjectOfType<PlayerController>().gameObject);
         DontDestroyOnLoad(FindObjectOfType<CameraFollow>().gameObject);
 
-        SceneManager.LoadScene("Main");
+        ChainsOfFate.Gerallt.GameManager gameManager = FindObjectOfType<ChainsOfFate.Gerallt.GameManager>();
+
+        if (gameManager == null)
+        {
+            SceneManager.LoadScene(managersScene, LoadSceneMode.Additive);
+        }
+
+        if (loadMainScene)
+        {
+            SceneManager.sceneLoaded += SceneManager_OnSceneLoaded;
+            SceneManager.LoadScene(mainScene, LoadSceneMode.Additive);
+        }
+    }
+
+    private void SceneManager_OnSceneLoaded(Scene newScene, LoadSceneMode sceneMode)
+    {
+        if (newScene.name == mainScene)
+        {
+            SceneManager.UnloadSceneAsync(thisScene);
+            SceneManager.SetActiveScene(newScene);
+            
+            SceneManager.sceneLoaded -= SceneManager_OnSceneLoaded;
+        }
     }
 }
