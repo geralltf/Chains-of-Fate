91ae05729e6deb5447f48e8ebe40998e68a1b4f2
Gerallt Franke <geralltf@gmail.com>
2022-04-17 17:28:13 +1000

    Experimental code

--- Assets/Team Members/TimP/PlayerSensor.cs
+++ Assets/Team Members/TimP/PlayerSensor.cs
@@ -5,18 +5,43 @@ using UnityEngine;
 
 public class PlayerSensor : MonoBehaviour
 {
+    public float detectionRadius;
     public PlayerController DetectedPlayer;
+
+    private WorldInfo worldInfo;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        worldInfo = FindObjectOfType<WorldInfo>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    // private void FixedUpdate()
+    // {
+    //     if (ChainsOfFate.Gerallt.GameManager.Instance.levelLoadingLock) return;
+    //
+    //     RaycastHit[] hits = Physics.SphereCastAll(transform.parent.position, detectionRadius, transform.parent.forward);
+    //     
+    //     DetectedPlayer = null;
+    //     
+    //     foreach (RaycastHit hit in hits)
+    //     {
+    //         PlayerController playerController = hit.transform.GetComponent<PlayerController>();
+    //         
+    //         if (playerController != null)
+    //         {
+    //             DetectedPlayer = playerController;
+    //
+    //             break;
+    //         }
+    //     }
+    // }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,7 +51,7 @@ public class PlayerSensor : MonoBehaviour
             DetectedPlayer = playerController;
         }
     }
-
+    
     private void OnTriggerExit(Collider other)
     {
         var playerController = other.GetComponent<PlayerController>();
