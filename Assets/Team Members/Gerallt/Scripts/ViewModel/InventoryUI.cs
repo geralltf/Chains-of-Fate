984552e5b4d2785c9f0744e803227db23f75a386
Gerallt Franke <geralltf@gmail.com>
2022-04-24 23:55:00 +1000

    Inventory item filtering by category: All, Weapon, Spell, or Item

--- Assets/Team Members/Gerallt/Scripts/ViewModel/InventoryUI.cs
+++ Assets/Team Members/Gerallt/Scripts/ViewModel/InventoryUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -14,6 +15,11 @@ namespace ChainsOfFate.Gerallt
         [SerializeField] private GameObject content;
         [SerializeField] private GameObject itemViewPrefab;
         [SerializeField] private TextMeshProUGUI textCharacterName;
+        [SerializeField] private Button buttonFilterWeapons;
+        [SerializeField] private Button buttonFilterSpells;
+        [SerializeField] private Button buttonFilterItems;
+        [SerializeField] private Button buttonFilterAll;
+        [SerializeField] private FilterBy filterBy = FilterBy.All;
         [SerializeField] private int xSpaceBetweenItem = 60;
         [SerializeField] private int ySpaceBetweenItems = 60;
         [SerializeField] private int xStart = 0;
@@ -24,6 +30,14 @@ namespace ChainsOfFate.Gerallt
         private Champion currentPlayer;
         private bool isToggling = false;
         
+        public enum FilterBy
+        {
+            Weapons,
+            Spells,
+            Items,
+            All
+        }
+        
         public void ItemButton_OnClick(IDescriptive item)
         {
             Debug.Log("TODO: Inventory Item use " + item.GetName() + " " + item.GetDescription());
@@ -31,8 +45,25 @@ namespace ChainsOfFate.Gerallt
 
         public void PopulateView()
         {
+            ClearItems();
+            
             List<IDescriptive> allItems = currentPlayer.GetInventory();
 
+            switch (filterBy)
+            {
+                case FilterBy.All:
+                    break;
+                case FilterBy.Weapons:
+                    allItems = allItems.Where(item => item is WeaponBase).ToList();
+                    break;
+                case FilterBy.Spells:
+                    allItems = allItems.Where(item => item is SpellBase).ToList();
+                    break;
+                case FilterBy.Items:
+                    allItems = allItems.Where(item => item is ItemBase).ToList();
+                    break;
+            }
+            
             int i = 0;
             
             for (int test = 0; test < 100; test++)
@@ -40,13 +71,12 @@ namespace ChainsOfFate.Gerallt
                 foreach (IDescriptive item in allItems)
                 {
                     GameObject itemUIInstance = Instantiate(itemViewPrefab, content.transform);
-                    //itemUIInstance.transform.localPosition = GetPosition(i); // Auto laid out by Grid Layout Group in Content area within Scroll View
 
                     Button button = itemUIInstance.GetComponentInChildren<Button>();
                     TextMeshProUGUI buttonText = itemUIInstance.GetComponentInChildren<TextMeshProUGUI>();
                     Image buttonImage = itemUIInstance.GetComponentInChildren<Image>();
                     
-                    buttonText.text = item.GetName();
+                    buttonText.text = item.GetName() + " " + i;
                     buttonImage.color = item.GetTint();
                     
                     button.onClick.AddListener(() =>
@@ -86,32 +116,46 @@ namespace ChainsOfFate.Gerallt
 
             if (visibility)
             {
-                ClearItems();
-                
                 PopulateView();
             }
         }
-        
-        private Vector3 GetPosition(int i)
-        {
-            float x = xStart + (xSpaceBetweenItem * (i % numberOfColumns));
-            float y = yStart + (-ySpaceBetweenItems * (i / numberOfColumns));
-            
-            return new Vector3(x, y,0f);
-        }
-        
+
         private IEnumerator ToggleVisibilityCoroutine()
         {
             yield return new WaitForSeconds(toggleInSeconds);
             SetVisibility(!view.activeInHierarchy);
             isToggling = false;
         }
-        
+
         private void Start()
         {
             currentPlayer = GameManager.Instance.GetPlayer();
 
             SetVisibility(false);
+            
+            buttonFilterWeapons.onClick.AddListener(() =>
+            {
+                filterBy = FilterBy.Weapons;
+                PopulateView();
+            });
+            
+            buttonFilterSpells.onClick.AddListener(() =>
+            {
+                filterBy = FilterBy.Spells;
+                PopulateView();
+            });
+            
+            buttonFilterItems.onClick.AddListener(() =>
+            {
+                filterBy = FilterBy.Items;
+                PopulateView();
+            });
+            
+            buttonFilterAll.onClick.AddListener(() =>
+            {
+                filterBy = FilterBy.All;
+                PopulateView();
+            });
         }
 
         private void Update()
