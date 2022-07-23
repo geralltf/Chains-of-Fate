0814b83d0e93fd77a7e63f059c569057284ca7c3
Kysis321 <72172415+Kysis321@users.noreply.github.com>
2022-07-23 13:33:10 +1000

    Dialouge Test Scene Rebuild

--- Assets/Team Members/Gerallt/Scripts/Model/FriendlyNPC.cs
+++ Assets/Team Members/Gerallt/Scripts/Model/FriendlyNPC.cs
@@ -220,7 +220,7 @@ namespace ChainsOfFate.Gerallt
                     }
                     else if (inDialogue && canExitDialogue)
                     {
-                       // StartCoroutine(EndDialogueCoroutine());
+                       StartCoroutine(EndDialogueCoroutine());
                     }
                 }
             }
